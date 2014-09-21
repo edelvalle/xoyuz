9ad0141cbecbc8f67053c985ba16fcba8947c092
Eddy Ernesto del Valle Pino <eddy@merchise.org>
2014-09-21 15:05:42 -0400

    Jump to 1.0.0.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-version = '0.2.8'
+version = '1.0.0'
 
 setup(
     name='xoyuz',
