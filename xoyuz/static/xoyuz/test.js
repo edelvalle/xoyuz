c822de5c034e126d7d75e417ef1169cfd0d3b11c
Eddy Ernesto del Valle Pino <eddy@merchise.org>
2014-09-20 21:59:06 -0400

    Remove tests, all must be rewritten.

--- xoyuz/static/xoyuz/test.js
+++
@@ -1,3 +0,0 @@
-random_number = function() {
-    return 42; // a number chosen randomly
-}
--- xoyuz/static/xoyuz/test2.js
+++
@@ -1,7 +0,0 @@
-factorial = function(x) {
-    if (x === 0) {
-        return 1;
-    } else {
-        return factorial(x - 1) * x;
-    }
-}
