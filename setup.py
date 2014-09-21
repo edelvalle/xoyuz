8daa631778d2737c808d41824b480b9676079989
Eddy Ernesto del Valle Pino <eddy@merchise.org>
2014-09-20 22:21:36 -0400

    Set ipython dependency version.

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
     zip_safe=False,
     install_requires=[
         'Django>=1.5,<1.8',
+        'ipython>=2.2,<2.3',
         'xoutil>=1.6.0,<1.7',
         'jsmin>=2.0.8,<3',
         'cssmin>=0.2.0,<1',
